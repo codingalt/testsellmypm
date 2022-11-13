6b404aea72e133f640827fc62fb2b620898b69d2
Faheem6765 <codingalt7@gmail.com>
2022-11-13 10:22:14 +0500

    second commit

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ app.get('/',(req,res)=>{
 
 const __dirname1 = path.resolve();
 
-// if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-//     app.use(express.static('client/build'));
-//     app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname1 + '/client/build/index.html'));
-//     });
-//    }
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+    app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+   }
 
 // app.use("/uploads", express.static("./uploads"));
 
